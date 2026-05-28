c26f36269ceece93f89ddc41a6ad597f6fa35ae8
Krishna <gmkrishna097@gmail.com>
2026-05-28 18:10:45 +0530

    adding eks provider block

--- Aws-infra/providers.tf
+++ Aws-infra/providers.tf
@@ -3,29 +3,29 @@ provider "aws" {
 }
 
 
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = module.eks.cluster_name
-# }
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
 
-# data "aws_eks_cluster" "cluster" {
-#   name = module.eks.cluster_name
-# }
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
   
-#   exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       command     = "aws"
-#       args = [
-#         "eks",
-#         "get-token",
-#         "--cluster-name",
+  exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
 
-#         data.aws_eks_cluster.cluster.name
-#       ]
-#     }
-# }
+        data.aws_eks_cluster.cluster.name
+      ]
+    }
+}
 
